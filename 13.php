ed478306387c7bfb276f6f055830381ccf8760b9
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-08-06 21:23:52 -0300

    add - exercício 13 feito

--- 13.php
+++ 13.php
@@ -8,5 +8,33 @@
 // Em seguida, crie outra função chamada descriptografarMensagem() capaz de
 // recuperar o texto original.
 
-function criptografarMensagem()
+function criptografarMensagem($mensagem, $deslocamento) {
+    $mensagemCriptografada = '';
+    $tamanho = strlen($mensagem); // pega o tamanho da mensagem
 
+    for ($i = 0; $i < $tamanho; $i++) {
+        $caractere = $mensagem[$i];
+
+        // verifica se o caractere é uma letra maiúscula
+        if (ctype_upper($caractere)) {
+            $mensagemCriptografada .= chr((ord($caractere) + $deslocamento - 65) % 26 + 65);
+        }
+        
+        // verifica se o caractere é uma letra minúscula
+        elseif (ctype_lower($caractere)) {
+            $mensagemCriptografada .= chr((ord($caractere) + $deslocamento - 97) % 26 + 97);
+        } else {
+            // mantém caracteres q não são do alfabeto inalterados
+            $mensagemCriptografada .= $caractere;
+        }
+    }
+
+    return $mensagemCriptografada;
+}
+
+function descriptografarMensagem($mensagemCriptografada, $deslocamento) { // descriptografar é o inverso de criptografar, ent reaproveita a função de criptografar
+    // aplicando o deslocamento "ao contrário" (26 - deslocamento)
+    return criptografarMensagem($mensagemCriptografada, 26 - $deslocamento);
+}
+
+?>
